9b10c11628abf8e1c7d9c1b3e0a8d3293537c83e
nikita.astashkin <nek1212121@gmail.com>
2023-10-03 21:45:50 +0300

    blog init

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    body = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.title} {self.author.username}'
