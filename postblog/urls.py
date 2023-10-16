d63b2cdc592d6ff7ac1a850a5f7fde6a71392b0e
Arina Untilova <untilovaao@mail.ru>
2023-10-16 19:36:15 +0300

    add modal auth form

--- postblog/urls.py
+++ postblog/urls.py
@@ -1,13 +1,20 @@
 from django.conf.urls.static import static
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
-from registration.views import user_login, user_registration
+from registration.views import user_login, user_registration, CustomLoginView, SignUpView, user_login_modal, user_registration_modal
 from postblog import settings
-from blogs.views import blog_view, post_detail, post_about
+from blogs.views import blog_view, post_detail, post_about, post_login_dialog
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('login/', user_login, name='login'),
+    path('login_dialog/', post_login_dialog, name='login_dialog'),
+    path('login_modal/', CustomLoginView.as_view(), name='login_modal'),
+    # path('login_modal/', user_login_modal, name='login_modal'),
+    path('signup_modal/', SignUpView.as_view(), name='signup_modal'),
+    # path('signup_modal/', user_registration_modal, name='signup_modal'),
     path('registration/', user_registration, name='registration'),
     #   path('translate/', blog_translate, name='translate'),
     path('', blog_view, name='main'),
--- registration/views.py
+++ registration/views.py
@@ -2,6 +2,17 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, resolve_url
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm, RegistrationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login, logout as auth_logout
+from django.urls import reverse_lazy
+from bootstrap_modal_forms.generic import (
+    BSModalLoginView,
+    BSModalCreateView,
+)
+from .forms import (
+    CustomUserCreationForm,
+    CustomAuthenticationForm
+)
 
 
 def user_login(request):
@@ -24,6 +35,35 @@ def user_login(request):
     registration_url = resolve_url('/registration')
     return render(request, 'registration/login.html', {'form': form, 'registration_url': registration_url})
 
+# для модального окна авторизации
+class CustomLoginView(BSModalLoginView):
+    authentication_form = CustomAuthenticationForm
+    template_name = "registration/login_form.html"
+    success_message = 'Success: You were successfully logged in.'
+    success_url = reverse_lazy('main')  # index
+
+
+# не используется
+def user_login_modal(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    resolved_url = resolve_url('/')
+                    return HttpResponseRedirect(resolved_url)
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+
+    form = LoginForm()
+    registration_url = resolve_url('/registration')
+    return render(request, 'registration/login_form.html', {'form': form, 'registration_url': registration_url})
+
 
 def user_registration(request):
     if request.method == 'POST':
@@ -44,3 +84,32 @@ def user_registration(request):
     else:
         form = RegistrationForm()
     return render(request, 'registration/registration.html', {'form': form})
+
+# для модального окна регистрации
+class SignUpView(BSModalCreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'registration/signup_form.html'
+    success_message = 'Success: Sign up succeeded. You can now Log in.'
+    success_url = reverse_lazy('main')
+
+
+# не используется
+def user_registration_modal(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            cd = form.cleaned_data
+            new_user.set_password(cd['password'])
+            new_user.save()
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    resolved_url = resolve_url('/')
+                    return HttpResponseRedirect(resolved_url)
+                else:
+                    return HttpResponse('Disabled account')
+    else:
+        form = RegistrationForm()
+    return render(request, 'registration/signup_form.html', {'form': form})
