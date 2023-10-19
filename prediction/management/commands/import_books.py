c6c669a08e39cfdd027844386b707585ac07289b
nikita.astashkin <nek1212121@gmail.com>
2023-10-19 19:21:25 +0300

    command end

--- prediction/management/commands/import_books.py
+++ prediction/management/commands/import_books.py
@@ -1,16 +1,19 @@
 import csv
 
 from django.core.management import BaseCommand
-from postblog.postblog.settings import BASE_DIR
-from postblog.prediction.models import Book_tbl
+from postblog.settings import BASE_DIR
+from prediction.models import Book_tbl
 
 
 class Command(BaseCommand):
     help = 'Set table data'
 
-    def handle(self):
-        with open(BASE_DIR + '/prediction/management/commands/books_data.csv') as csv_file:
+    def handle(self, *args, **kwargs):
+        with open(str(BASE_DIR) + '/prediction/management/commands/books_data.csv') as csv_file:
             data = csv.DictReader(csv_file, delimiter=',')
             for book in data:
+                for key, value in book.items():
+                    if not value:
+                        book[key] = None
                 Book_tbl.objects.create(**book)
 
